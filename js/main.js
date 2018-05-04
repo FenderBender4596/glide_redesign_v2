6d25c0fbcef2355c0b336dd21f140a373512a567
Jacob Bender <jakebender@n3-18-129.dhcp.drexel.edu>
2018-05-04 13:56:17 -0400

    front_page_styling

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+// 	$(".mobile-menu").on('click', function(){
+//         console.log('menu_toggled');
+// 		$(".mobile-dropdown").slideToggle("fast");
+// 	});
+// });
+
+// function stext() {
+//     var x = document.getElementById("pastLink");
+//     var y = document.getElementById("currentLink");
+//     x.style.color = 'red';
+//     y.style.color = 'black';
+// }
+
+
+// var togByClass = document.getElementById('togClass');
+// var addClassToDiv = document.getElementById("mobile");
+
+// togByClass.addEventListener("click", function () {
+//     toggleMenu();
+// });
+
+// function toggleMenu() {
+//     console.log('toggled menu');
+//     addClassToDiv.classList.toggle("mobile-menu");
+//   }
